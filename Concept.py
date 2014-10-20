fe7dc15c0ae45b05a24d6d824ae4a91e02283674
amne51ac <th3amne51ac@gmail.com>
2014-10-19 22:51:15 -0500

    Let's get this party started
    Because I'm always useless at commenting

---
+++ Concept.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 19 22:45:51 2014
+
+@author: amne51ac
+
+All content licensed under GPL V2 unless otherwise noted or required.
+
+This is the concept document for the Mainframe Distributed Computer Project
+
+For more information please contact amne51ac (twitter or @gmail.com)
+
+
+###############################################################################
+
+
+
+"""
+
